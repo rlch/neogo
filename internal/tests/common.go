6f82451350d8ff8c0a0d79127d2068cddc61b788
RJM <rjmbowie@gmail.com>
2024-03-20 17:33:32 +1100

    chore: Add multi-polymorphic example

--- internal/tests/common.go
+++ internal/tests/common.go
@@ -104,10 +104,33 @@ type Organism interface {
 	internal.IAbstract
 }
 
+type Pet interface {
+	internal.IAbstract
+	Organism
+	IsCute() bool
+}
+
 type BaseOrganism struct {
 	internal.Abstract `neo4j:"Organism"`
 	internal.Node
 	Alive bool `json:"alive"`
+}
+
+type BasePet struct {
+	internal.Abstract `neo4j:"Pet"`
+	BaseOrganism
+
+	Cute bool `json:"cute"`
+}
+
+func (b BasePet) IsCute() bool {
+	return b.Cute
+}
+
+func (b BasePet) Implementers() []internal.IAbstract {
+	return []internal.IAbstract{
+		&Dog{},
+	}
 }
 
 func (b BaseOrganism) Implementers() []internal.IAbstract {
@@ -123,6 +146,6 @@ type Human struct {
 }
 
 type Dog struct {
-	BaseOrganism `neo4j:"Dog"`
-	Borfs        bool `json:"borfs"`
+	BasePet `neo4j:"Dog"`
+	Borfs   bool `json:"borfs"`
 }
